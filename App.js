3c2096cd8a18507c9cac53268ba3715449ab4eda
gaddistturnerg2 <46493521+gaddistturnerg2@users.noreply.github.com>
2019-04-26 11:10:01 -0400

    Ajax

---
+++ App.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = 8000;
+
+const indexRouter = require('./routes/index');
+
+app.use(express.urlencoded({extended:false}));
+
+app.use('/',express.static('public'));
+
+app.set('views',path.join(__dirname,"views"));
+app.set('view engine', 'pug');
+
+app.use('/', indexRouter);
+
+
+
+app.listen(port);
